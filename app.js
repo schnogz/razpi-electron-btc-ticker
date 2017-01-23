eae249ac1fdcb81a6dce111cea277e40c580eb6d
Andrew Schneider <andrew.mark.schneider@gmail.com>
2017-01-23 15:29:21 -0600

    adding socket connection for realtime mined block notifications

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var exchange = require('blockchain.info/exchange');
+var Socket = require('blockchain.info/Socket');
+var btcMath = require('./utils/btcMath');
 
+var socket = new Socket();
+
+// listens for new mined blocks
+socket.onBlock(function(newBlock) {
+	console.log('New block mined by ' + newBlock.foundBy.description + '.' +
+		' Halvening is now '+ btcMath.getHalveningProgress(newBlock.height) + '% complete!');
+});
 
 // gets current BTC price
 var getBtcPrice = function() {
 	exchange.getTicker({currency: 'USD'})
 		.then(function(price) {
-			console.log('$%d', price.last);
+			console.log('BTC Price: $%d', price.last);
 		});
 };
+
+getBtcPrice();
 
 // logs price every 30 seconds
 setInterval(getBtcPrice, 30000);
