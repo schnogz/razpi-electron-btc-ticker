f635d91ea8f352ee12a2cdfe51adbbf4aa285ea8
Andrew Schneider <andrew.mark.schneider@gmail.com>
2017-02-10 21:02:55 -0600

    adding support for API code

--- controllers/api.js
+++ controllers/api.js
@@ -1,15 +1,16 @@
 var btc = require('blockchain.info/exchange');
 var stats = require('blockchain.info/statistics');
+var apiCode = require('./apiCode');
 
 exports.getCurrentPrice = (req, res) => {
-  btc.getTicker({ currency: 'USD' })
+  btc.getTicker({ currency: 'USD', apiCode: apiCode })
     .then((resp) => {
       res.send(resp);
     });
 };
 
 exports.getHistoricalPriceChart = (req, res) => {
-  stats.getChartData('market-price', { timespan: req.query.timespan })
+  stats.getChartData('market-price', { timespan: req.query.timespan, apiCode: apiCode })
     .then((resp) => {
       res.send(resp);
   })
