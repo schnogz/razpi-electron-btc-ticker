47aa4cce2510ea6e6cf01a90ef73e8a4098f51c8
Andrew Schneider <andrew.mark.schneider@gmail.com>
2017-02-12 22:33:41 -0600

    remove installer since it needs to be ARM on pi3

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
 
 // keep reference to window object otherwise it will be
 // closed automatically when the JS object is garbage collected.
@@ -11,14 +10,14 @@ function createWindow () {
   app.server = require(__dirname + '/server.js')();
 
   // configure window and load app
-  if (isDev) {
+  if (process.env.NODE_ENV == 'production') {
+    win = new BrowserWindow({ fullscreen: true });
+  } else {
     win = new BrowserWindow({
       width: 1200,
       height: 1000
     });
     win.webContents.openDevTools();
-  } else {
-    win = new BrowserWindow({ fullscreen: true });
   }
 
   win.loadURL('http://localhost:3000');
